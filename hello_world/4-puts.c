34efeca9793b74b1031c03b25018245de028b156
Tchepekino-code <12193@holbertonstudents.com>
2025-10-21 13:03:00 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int	main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 int puts(const char *string);
 	puts("\"Programming is like building a multilingual puzzle");
