83c3fa68ff361f5e0c754e9d4a2aa5492b43b679
Tchepekino-code <12193@holbertonstudents.com>
2025-10-22 19:19:23 +0200

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  for (char c = 'a'; c <= 'z'; c++)
+    putchar(c);
+  for (char b = 'A'; b <= 'Z'; b++)
+    putchar(b);
+  putchar('\n')
+
+  return (0)
+}
