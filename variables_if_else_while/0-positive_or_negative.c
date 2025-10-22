56f98c1e98bdc72daec82cf2d0a8f794cafd5d9f
Tchepekino-code <12193@holbertonstudents.com>
2025-10-22 11:12:00 +0200

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0){
-	  printf("%ld is positive", n)}
+	  printf("%d is positive\n", n);
+	}
 	if (n == 0){
-	  printf("%lu is zero", n)}
+	  printf("%d is zero\n", n);
+	}
 	if (n < 0){
-	  printf("ld is negative", n}
+	  printf("d is negative\n", n);
+	}
 	return (0);
 }
