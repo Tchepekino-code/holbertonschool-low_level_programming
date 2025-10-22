fe3c20d2889c867c13cb16b3618fe3f664ed036a
Tchepekino-code <12193@holbertonstudents.com>
2025-10-22 12:51:49 +0200

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there *//**
+/**
  * main - Entry point
  *
- * Return: n ,p, or 0)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
