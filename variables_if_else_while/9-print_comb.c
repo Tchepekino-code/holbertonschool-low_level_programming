db7704835485019dbc76e3d827980bcf32c8710b
Tchepekino-code <12193@holbertonstudents.com>
2025-10-22 21:34:15 +0200

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+if(x == 57)
+break;
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
