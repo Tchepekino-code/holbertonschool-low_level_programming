5b40f879c016a9eff9585eb1fcd02d5779f60f15
Tchepekino-code <12193@holbertonstudents.com>
2025-10-23 16:26:04 +0200

    0

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,7 +1,25 @@
-#include <unistd.h>  
-#include "main.h"    
+/* Déclaration manuelle de write() pour éviter les includes */
+int write(int fd, const void *buf, unsigned int count);
 
+/**
+ * _putchar - écrit un caractère sur la sortie standard
+ * @c: caractère à afficher
+ *
+ * Return: 1 en cas de succès, -1 en cas d'erreur
+ */
 int _putchar(char c)
 {
     return write(1, &c, 1);
+}
+
+/* Exemple de test dans le même fichier */
+int main(void)
+{
+    char phrase[] = "Bonjour!\n";
+    int i;
+
+    for (i = 0; phrase[i] != '\0'; i++)
+        _putchar(phrase[i]);
+
+    return 0;
 }
