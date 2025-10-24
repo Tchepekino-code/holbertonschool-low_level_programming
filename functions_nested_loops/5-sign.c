0a3deddb1da38de1748781e17fecc908236a5a4d
Tchepekino-code <12193@holbertonstudents.com>
2025-10-24 15:53:02 +0200

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *int  print_sign - checks for alphabtic character
+ * @c: The character to check
+ *
+ * Returns 1 and prints + if n is greater than zero
+ *Returns 0 and prints 0 if n is zero
+ *Returns -1 and prints - if n is less than zero
+ */
 int print_sign(int n)
 {
 if (n > 0) {
@@ -13,4 +21,5 @@ return (0);
 else {
 _putchar('-');
 return (-1)
+}
 }
