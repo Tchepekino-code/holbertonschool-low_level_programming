6a4525c4f7a0f05d80b7beee5dfa257cb80e1ec3
Tchepekino-code <12193@holbertonstudents.com>
2025-10-24 13:27:01 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 int i; char c;
 
-for (i = 1; i <= 10; i++){
+for (i = 1; i <= 10; i++)
+{
 for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
