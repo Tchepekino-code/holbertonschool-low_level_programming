f97a76bde99f7abe840ce361d1247b94046e7f7b
Tchepekino-code <12193@holbertonstudents.com>
2025-10-24 13:07:43 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
  */
 void print_alphabet_x10(void)
 {
-  
 int i;
-for (i = 0; i <= 10; i++)
-print_alphabet();
-
+char c;
+for (i = 0; i <= 10; i++){
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+}
+ _putchar('\n');
 }
