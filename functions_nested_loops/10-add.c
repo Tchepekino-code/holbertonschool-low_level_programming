db8210ff2741f1e84f2c03650584f1b1b5bc4557
Tchepekino-code <12193@holbertonstudents.com>
2025-10-26 01:43:17 +0200

    10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * add - adds two integers and returns the result
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the sum of a and b
+ */
+
 int add(int i, int j)
 {
 return (i + j);
