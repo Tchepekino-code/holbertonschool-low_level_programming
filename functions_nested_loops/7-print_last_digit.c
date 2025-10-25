0379e17343684e98870725ffb98aaa555fc1c541
Tchepekino-code <12193@holbertonstudents.com>
2025-10-25 15:10:33 +0200

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - ecrit le dernier chiffre d'un nombre
+ * @i: le nombre dont on veut le dernier chiffre
+ *
+ * Return: le dernier chiffre de i
+ */
+int print_last_digit(int i)
+{
+int l = i % 10;
+return(l);
+}
