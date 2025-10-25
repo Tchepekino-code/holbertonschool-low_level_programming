1271769e08bea1deeb630e2eb156dcd51ede8485
Tchepekino-code <12193@holbertonstudents.com>
2025-10-25 20:26:38 +0200

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Description: This function prints every minute of the day
+ * starting from 00:00 to 23:59.
+ */
+
 void jack_bauer(void)
 {
 
