61ed9f50eb8a9e036b866fda3a02f92ac9e6b8a5
Tchepekino-code <12193@holbertonstudents.com>
2025-10-25 14:47:21 +0200

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @r: The character to check
+ *
+ * Return: absolute value
+ */
+int _abs(int)
+{
+int i;
+if (i >= 0)
+ {
+ _abs = i;
+ }
+ else {
+ i = -i;
+}
+}
