32560f54e61f4af7357e8a781c8a13f4acc26780
Tchepekino-code <12193@holbertonstudents.com>
2025-10-25 21:16:02 +0200

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,16 +4,24 @@ void times_table(void){
 
   int i;
   int j;
-
+  for (k = 0; k <= 9; k++){
+    _putchar('0')
+      _putchar(',');
+    -putchar(' '),
+      }
+  _putchar('\n');
   for (i = 0; i <= 9; i++){
     _putchar('0');
     _putchar(',');
     _putchar(' ');
+    {
     for (j = 1; j <= j * 9 ; j = j + j){
       _putchar(j +'0');
       _putchar(',');
       _putchar(' ');
     }
+    }
+    _putchar('\n');
   }
   
 
