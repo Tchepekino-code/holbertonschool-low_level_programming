f6d0b738945081b59d067ff695295a916a277e7d
Tchepekino-code <12193@holbertonstudents.com>
2025-10-26 13:24:43 +0100

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,25 +2,37 @@
 
 void print_to_98(int n)
 {
-  while (n < 98)
-    {
-    _putchar(n + '0');
-    n++;
-    if (n == 97)
-      break;
-    _putchar(',');
-    _putchar(' ');
-    }
-  while (n > 98)
-    {
-      _putchar(n + '0');
-      n++;
-      if (n == 99)
-	break;
-      _putchar(',');
-      _putchar(' ');
-    }
-  for (n = 98; ; n++)
-    _putchar(n + '0');
+for (; n <= 97; n++){
+if (n < 0){
+_putchar('-');
+n = -n;
+if (n / 10 != 0)
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+n = -n;
+}
+else if (n >= 0)
+{
+if (n / 10 != 0)
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+_putchar(',');
+_putchar(' ');
+}
+
+for (; n >= 99; n--)
+{
+if (n / 100 != 0)
+_putchar(n / 100 + '0');
+if (n / 10 != 0)
+_putchar((n / 10 )% 10 + '0');
+_putchar(n % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('9');
+_putchar('8');
+_putchar('\n');
 }
 
