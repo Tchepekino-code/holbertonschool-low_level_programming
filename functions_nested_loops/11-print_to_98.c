a46305537520bb9ac4bc8e907ab95d07f21ded8f
Tchepekino-code <12193@holbertonstudents.com>
2025-10-26 11:46:33 +0100

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+  if (n < 98; n++){
+    _putchar(n + '0');
+    _putchar(',');
+    _putchar(' ');
+  }
+  else if (n > 98; n--){
+     _putchar(n + '0');
+    _putchar(',');
+    _putchar(' ');
+  }
+  else
+    _putchar('98');
+}
+}
