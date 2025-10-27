739865e63ad054f872f783ff360f200bb358cc37
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 15:01:41 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,24 @@ void print_diagonal(int n)
 {
 int i, j;
 
-if (n != 0)
+if (n != 0 || n != 0)
 {
-  for (i = 0; i < n; i++){
+for (j = 0; j < (n - 1); j++)
+_putchar(' ');
+ 
+for (i = 0; i < n; i++)
 _putchar(92);
 _putchar('\n');
-  }
 
- for (j = 0; j < (n - 1); j++)
-_putchar(' ');
+
 
 
 }
+else if (n ==1){
+_putchar(92);
+_putchar('\n');
+}
 
-else
+else 
 _putchar('\n');
 }
