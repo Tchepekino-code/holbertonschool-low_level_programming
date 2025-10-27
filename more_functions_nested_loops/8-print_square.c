7c5a970b36d6d9ea0c7bbfddc90a421b62592b1b
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 16:07:52 +0100

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,19 @@
 
 void print_square(int size)
 {
-  int i;
-  int j;
-  if (size > 0)
-    {
-    for (j = 0; j < size; j++)
-      {
-    for (i = 0; i < size ; i++)
-      {
-	_putchar('#');
-      }
-    _putchar('\n');
-    }
-    _putchar('\n');
-  }
-  else
-    _putchar('\n');
+int i;
+int j;
+if (size > 0)
+{
+for (j = 0; j < size; j++)
+{
+for (i = 0; i < size ; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
