ba8eda96600c0b62891e8fb919c146a73af2b6c3
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 10:29:46 +0100

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _isupper - Vérifie si un caractère est une lettre majuscule
+ * @c: le caractère à tester
+ *
+ * Return: 1 si c est une majuscule, 0 sinon
+ */
 int _isupper(int c)
 {
 if (c <= 90 && c >= 65)
