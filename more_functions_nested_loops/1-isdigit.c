dd6d896f63271adc3b11bee17a683bd624b5a867
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 10:37:36 +0100

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * _isdigit - Vérifie si un caractère est un chiffre entre 0 et 9
+ * @c: le caractère à tester
+ *
+ * Return: 1 si c est un chiffre entre 0 et 9, 0 sinon
+ */
+
 int _isdigit(int c)
 {
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
   
