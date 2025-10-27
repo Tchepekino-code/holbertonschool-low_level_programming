4d97259f98acf11675366f5c530552b2471158f3
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 11:21:22 +0100

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - prints, from 0 to 9, followed by a new line.
  *
  */
 
 void print_most_numbers(void)
 {
-int i ;
+int i;
 for (i = 48; i <= 57; i++)
 {
-if (i == 50 || i == 52 )
+if (i == 50 || i == 52)
 continue;
 _putchar(i);
 }
