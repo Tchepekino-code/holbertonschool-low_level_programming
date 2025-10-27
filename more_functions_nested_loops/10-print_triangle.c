94d7f2c87386b619b6722eb83dc40d538e17d838
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 21:47:27 +0100

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle -prints a triangle, followed by a new line.
+ * @size: is the size of the triangle
+ */
+void print_triangle(int size)
+{
+  int n;
+  
+  if (size > 0){
+    for (n = (size - 1) ; n >= 0; n--)
+      _putchar(' ');
+
+    for (m = 0; m <= size; m++)
+      _putchar('#');
+    _putchar('\n);
+
+
+    
+  }
+  else
+    _putchar('\n');
+}
