a8ea9b571755908d9495b76f710a54102fb26186
Tchepekino-code <12193@holbertonstudents.com>
2025-10-27 15:58:29 +0100

    7

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: the size of the square
+ *
+ */
+
+void print_square(int size)
+{
+  int i;
+  int j;
+  if (size > 0){
+    for (j = 0; i < size; j++){
+    for (i = 0; i < size ; i++)
+      {
+	_putchar('#');
+      }
+    }
+    _putchar('\n');
+
+
+
+  }
+  else
+    _putchar('\n');
+}
