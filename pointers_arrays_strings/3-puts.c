cc153407e7a920fd59040f9eef49065fbefa024d
Tchepekino-code <12193@holbertonstudents.com>
2025-10-28 15:24:35 +0100

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - retourne la longueur d'une chaîne de caractères
+ * @*str: la chaîne qu'on veut ecrire
+ *
+ */
+void _puts(char *str)
+{
+int c = 0;
+while (str[c] != '\0'){
+_putchar('str[c]');
+c++;
+}
+
+
+}
