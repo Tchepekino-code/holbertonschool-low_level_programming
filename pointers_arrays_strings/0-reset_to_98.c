e80efdb30685c79a633889b3b8d3060ee7487ae8
Tchepekino-code <12193@holbertonstudents.com>
2025-10-28 14:10:52 +0100

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Vérifie si un caractère est une lettre majuscule
+ * @*n:  parameter and updates the value it points to to 98
+ *
+ */
+void reset_to_98(int *n)
+{
+  int *p = &n;
+  *p = 98;
+
+
+}
