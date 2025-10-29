e8dd9ad2802bfc65b4b48592c3dabd7a84538f21
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 10:42:23 +0100

    4

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - inverse une chaîne de caractères
+ * @s: pointeur vers la chaîne de caracteres
+ *
+ */
+void rev_string(char *s)
+{
+  int i = 0;
+  int l;
+  char tmp;
+
+  while (s[l] != 0)
+    l++;
+  l--;
+  while (l >= i){
+    tmp = s[i];
+    s[i] = s[l];
+    s[l] = tmp;
+    i++;
+    l--;
+	    }
+}
