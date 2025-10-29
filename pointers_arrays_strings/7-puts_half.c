6aed7ed593d20664de7bb9a58782822c370c8f2c
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 15:28:24 +0100

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,30 +5,22 @@
  * @str: pointeur vers la chaîne 
  *
  */
-#include "main.h"
-
-/**
- * puts_half - affiche la deuxième moitié d'une chaîne
- * @str: pointeur vers la chaîne
- */
 void puts_half(char *str)
 {
-    int l = 0;
-    int m;
+int l = 0;
+int m;
 
-    while (str[l] != '\0')
-        l++;
+while (str[l] != '\0')
+l++;
 
-    if (l % 2 == 0)
-        m = l / 2;
-    else
-        m = (l - 1) / 2 + 1;
-
-    while (str[m] != '\0')
-    {
-        _putchar(str[m]);
-        m++;
-    }
-
-    _putchar('\n');
+if (l % 2 == 0)
+m = l / 2;
+else
+m = (l - 1) / 2 + 1;
+while (str[m] != '\0')
+{
+_putchar(str[m]);
+m++;
+}
+_putchar('\n');
 }
