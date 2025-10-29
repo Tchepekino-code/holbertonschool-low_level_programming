548a7a9fefc04df37f18b1ef94635388c5db5ae8
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 13:16:55 +0100

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,13 @@
  */
 void puts2(char *str)
 {
-  int l = 0;
+int l = 0;
 
-  while(str[l] != '\0')
-    {
-      if (l % 2 == 0)
-      _putchar(str[l]);
-      l+=1;
-    }
-  _putchar('\n');
+while(str[l] != '\0')
+{
+if (l % 2 == 0)
+_putchar(str[l]);
+l+=1;
+}
+_putchar('\n');
 }
