e7555758cf7ce5255cf70a8ee920e38536175a3c
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 13:47:21 +0100

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,23 +12,21 @@ void puts_half(char *str)
   int n;
 
   while (str[l] != ('\0'))
+    {
     l += 1;
   l -= 1;
   m = (l / 2);
   n = (m - 1);
   if ((m % 2) == 0)
     {
-      for (m <= l)
-	{
+      for (m = (l / 2);m <= l ;m++)
 	_putchar(str[m]);
-	}
-      m += 1;
     }
       else
 	{
-	  for (n <= l)
+	  for (n = (m + 1);n <= l;n++)
 	    _putchar(str[n]);
-		     n += 1;
 	 }
-      _putchar('\n');
+}
+  _putchar('\n');
 }
