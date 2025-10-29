ae7bdf6ee9e236e1130800a68eb825a6fdb74f8d
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 10:55:15 +0100

    4

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,18 @@
  */
 void rev_string(char *s)
 {
-  int i = 0;
-  int l = 0;
-  char tmp;
+int i = 0;
+int l = 0;
+char tmp;
 
-  while (s[l] != 0)
-    l++;
-  l--;
-  while (l > i){
-    tmp = s[i];
-    s[i] = s[l];
-    s[l] = tmp;
-    i++;
-    l--;
-	    }
+while (s[l] != 0)
+l++;
+l--;
+while (l > i){
+tmp = s[i];
+s[i] = s[l];
+s[l] = tmp;
+i++;
+l--;
+}
 }
