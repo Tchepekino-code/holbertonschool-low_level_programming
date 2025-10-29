fb08e2be09e9dbce22c80c3361674e6ed581048d
Tchepekino-code <12193@holbertonstudents.com>
2025-10-29 10:22:08 +0100

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,15 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
+	int len = 0;
+	while (s[len] != '\0')
+	len++;
 
-   
-    while (s[len] != '\0')
-        len++;
+	while (len > 0)
+	{
+	len--;
+	_putchar(s[len]);
+	}
 
-    while (len > 0)
-    {
-        len--;
-        _putchar(s[len]);
-    }
-
-    _putchar('\n');
+	_putchar('\n');
 }
