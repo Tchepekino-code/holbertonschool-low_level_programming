475f18614304ed6e60d585f524ac9eb010ceaca7
Tchepekino-code <12193@holbertonstudents.com>
2025-10-30 12:03:12 +0100

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,13 @@
  */
 void print_array(int *a, int n)
 {
-  int i;
-    
-  for (i = 0; i < n; i += 1)
-    {
-      printf("%d", *(a + i));
-    if (i < (n - 1))
-      printf(", ");
-    }
-  printf("\n");
+int i;
+
+for (i = 0; i < n; i += 1)
+{
+printf("%d", *(a + i));
+if (i < (n - 1))
+printf(", ");
+}
+printf("\n");
 }
