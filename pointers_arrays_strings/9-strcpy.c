5e5d5ed09cf487f135799fe4901935b687e90343
Tchepekino-code <12193@holbertonstudents.com>
2025-10-30 12:47:42 +0100

    8

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - retourne la longueur d'une chaîne de caractères
- * @s: pointeur vers la chaîne dont on veut connaître la longueur
+ * strcpy - copies the string pointed to by src
+ * @dest: pointer to dest
+ * @src: the string pointed to
  *
- * Return: la longueur de la chaîne (nombre de caractères avant '\0')
+ * Return:  value: the pointer to dest
  */
+char *_strcpy(char *dest, char *src)
+{
+  int i;
+  
+  for (i = 0; i <= '\0'; i++)
+    dest[i] = src[i];
+
+
+  return (*dest);
+}
