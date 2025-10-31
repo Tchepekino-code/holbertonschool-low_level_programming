3b907c9bf2a2f34e5191ddf8a33bc5f18fe3359a
Tchepekino-code <12193@holbertonstudents.com>
2025-10-31 15:42:11 +0100

    02

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
   char *p = dest;
 
   while (n-- > 0 && *src)
-    {
-      *p = *src;
-      p++;
-      src++;
-}
+      *p++ = *src++;
+  
   while (n-- > 0)
     *p++ = '\0';
   
