2a488ee89d35d355b62c498ad40691031e44b05b
Tchepekino-code <12193@holbertonstudents.com>
2025-10-31 12:53:19 +0100

    00

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - retourne la longueur d'une chaîne de caractères
+ * @dest: string to append
+ * @src: destinated string
+ *
+ * Return: char, pointeur vers la resukt string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+
+while (*src != '\0')
+{
+p++;
+}
+while (*dest != '\0')
+{
+*src = *dest;
+src++;
+dest++;
+}
+*src = '\0';
+return (*dest);
+}
