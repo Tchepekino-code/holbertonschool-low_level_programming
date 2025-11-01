4a4a40c852af2bdcabfe2731e71140cd3d3de68e
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 17:45:53 +0100

    05

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper -changes all lowercase letters of a string to uppercase.
+ * @char: pointeur vers la chaîne 
+ *
+ * Return: Uppercase string
+ */
+char *string_toupper(char *);
+{
+  int i;
+
+  for (i = 0; i < '\0'; i++)
+    {
+      if (char[i] >= 97 && char [i] <= 122)
+	{
+	tmp = char[i];
+      char[i] = (tmp -26);
+	}
+	else
+	  continue;
+    }
+  return (char);
+}
