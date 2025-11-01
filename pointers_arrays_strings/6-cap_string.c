6d7a0193ac5cd659c0c8a9fc03e3ff69e796ceb9
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 18:45:57 +0100

    06

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,28 @@
 
 /**
  * char *cap_string(char *) - function that capitalizes all words of a string.
- * @str: chaine a capitaliser
+ * @s: chaine a capitaliser
  *
  * Return: chaine capitalisée
  */
 char *cap_string(char *s)
 {
-    int i = 0;
-    char sep[] = " \t\n,;.!?\"(){}";
+int i = 0;
+char sep[] = " \t\n,;.!?\"(){}";
+ 
+if (s[0] >= 'a' && s[0] <= 'z')
+s[0] -= 32;
 
-    /* première lettre de la chaîne */
-    if (s[0] >= 'a' && s[0] <= 'z')
-        s[0] -= 32;
+while (s[i] != '\0')
+{
+int j;
 
-    while (s[i] != '\0')
-    {
-        int j;
-
-        for (j = 0; sep[j] != '\0'; j++)
-        {
-            /* si s[i] est un séparateur et s[i+1] est une minuscule */
-            if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-                s[i + 1] -= 32;
-        }
-        i++;
-    }
-
-    return s;
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+s[i + 1] -= 32;
+}
+i++;
+}
+ return (s);
 }
