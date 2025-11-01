cb0d70fa8ce1336fb6d17fc0ee2ed1f4b34c2fee
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 18:40:01 +0100

    06

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,18 @@
  */
 char *cap_string(char *s)
 {
-  int i = 0;
+    int i = 0;
+    int j;
+    char sp[] = ",;.!?\"(){}";
 
-  while (s[i] != '\0')
+    while (s[i] != '\0')
     {
-      if (s[i] = (44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125) && s[i+2] >= 'a' && s[i+2] <= 'z')
-	{
-	  s[i+2] = s[i+2] - 32;
-	  i++;
-	  }
-      else
-	i++;
-
+        for (j = 0; sp[j] != '\0'; j++)
+        {
+            if (s[i] == sp[j] && s[i + 2] >= 'a' && s[i + 2] <= 'z')
+                s[i + 2] -= 32;
+        }
+        i++;
     }
-  return (s);
+    return (s);
 }
