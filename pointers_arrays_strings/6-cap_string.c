0032c306e022746acd77b99e5d17384be0f73250
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 18:26:05 +0100

    06

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * char *cap_string(char *) - function that capitalizes all words of a string.
+ * @str: chaine a capitaliser
+ *
+ * Return: chaine capitalisée
+ */
+char *cap_string(char *str)
+{
+  int i = 0;
+
+  while ([si] != '\0')
+    {
+      if (s[i] = (44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 ||125) && s[i+2] >= 'a' && s[i+2] <= 'z')
+	{
+
+	  s[i+2] = s[i+2] - 32;
+	i++
+	  }
+      else
+	i++;
+
+    }
+  return (str);
+}
