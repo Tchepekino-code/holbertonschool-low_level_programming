cc83bda4b5f3921a754691cfa746998241e14208
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 18:04:49 +0100

    05

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 /**
  * string_toupper -changes all lowercase letters of a string to uppercase.
- * @char: pointeur vers la chaîne 
+ * @char: pointeur vers la chaîne
  *
  * Return: Uppercase string
  */
