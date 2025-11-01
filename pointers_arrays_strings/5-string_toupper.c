1eb051394b7801149248edbaf1a0f52b759b12a4
Tchepekino-code <12193@holbertonstudents.com>
2025-11-01 18:04:14 +0100

    05

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,11 +8,11 @@
  */
 char *string_toupper(char *str)
 {
-int i;
+int i = 0;
 
 while (str[i] != '\0')
 {
-if (str[i] >= 'a' && str[i] >= 'z')
+if (str[i] >= 'a' && str[i] <= 'z')
 str[i] = str[i] - 32;
 i++;
 }
