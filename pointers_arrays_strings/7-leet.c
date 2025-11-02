d7dd9dc9f0fdbb041820ec7ed8c3762b7430d926
Tchepekino-code <12193@holbertonstudents.com>
2025-11-02 20:06:35 +0100

    07

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == letters[j])
+{
+s[i] = numbers[j];
+break;
+}
+}
+}
+return (s);
+}
