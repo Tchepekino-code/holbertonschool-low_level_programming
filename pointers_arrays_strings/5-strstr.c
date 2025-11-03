1693ce5931e1aec822973a6e4acd1580cab2c9ab
Tchepekino-code <12193@holbertonstudents.com>
2025-11-03 23:03:20 +0100

    005

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: the main string to be searched
+ * @needle: the substring to look for
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+if (!*needle)
+    return (haystack);
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+    for (j = 0; needle[j] != '\0'; j++)
+    {
+        if (haystack[i + j] != needle[j])
+            break;
+    }
+    if (needle[j] == '\0')
+        return(&haystack[i]);
+}
+return (NULL);
+}
