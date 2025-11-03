561179dab862147df976fec3198ff87820238524
Tchepekino-code <12193@holbertonstudents.com>
2025-11-03 17:56:56 +0100

    002

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates the first occurrence of a character in a string
+ * @s: pointer to the string to be searched
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+if (c == '\0')
+{
+return (s + i);
+}
+return (NULL);
+}
