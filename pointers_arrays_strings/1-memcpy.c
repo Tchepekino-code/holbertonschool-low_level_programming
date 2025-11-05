d0b3259db3f5b52886e86c6e8ca8188fa64701d4
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 10:42:22 +0100

    01

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,13 +11,14 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
-    char *start = dest;
+
+    if (n == 0 || dest == src)
+        return dest;
 
     for (i = 0; i < n; i++)
     {
-        *dest = *src;
-        dest++;
-        src++;
+        dest[i] = src[i];
     }
-    return start;
+    return dest;
+}
 
