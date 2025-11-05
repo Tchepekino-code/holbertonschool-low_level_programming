f296a7ff9643db1017f3427c448b008709d5ef83
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 15:58:15 +0100

    003

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,13 +8,10 @@
  */
 int factorial(int n)
 {
-int f;
+if (n < 0)
+    return (-1);
+if (n == 0)
+    return (1);
 
-    if (n < 0)
-        return (-1);
-    else if (n == 0)
-        return (1);
-
-        f = n * factorial(n - 1);
-        return (f);
+return (n * factorial(n - 1));
 }
