3b89169e5eaa2a066639ace2683034a7799a2b17
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 20:43:05 +0100

    000

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,17 +3,15 @@
 /**
  * _puts_recursion - prints a string followed by a newline
  * @s: pointer to the string to print
- *
  */
 void _puts_recursion(char *s)
 {
 
 if (!*s)
     _putchar('\n');
+    return;
 
-else
-{
     _putchar(*s);
     _puts_recursion(s + 1);
-}
+
 }
