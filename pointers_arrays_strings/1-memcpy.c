3414dff9e3366b581ac29cde8a78ecc624bd8db2
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 10:37:47 +0100

    01

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: pointer to the destination memory area dest
  */
-char *memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
     char *start = dest;
