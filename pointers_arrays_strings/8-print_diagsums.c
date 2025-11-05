385aee98f71586a49e99b2513009e34bb4e90f52
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 11:04:31 +0100

    08

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,18 +10,14 @@ include "stdio.h"
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int j;
-int sum1 = 0;
-int sum2 = 0;
+    int i;
+    int sum1 = 0;
+    int sum2 = 0;
 
-for (i =0; i < size; i++)
-{
-    sum1 += a[i * size + i];
-}
-for (j = size - 1; j >= 0; j--)
-{
-    sum2 += a[j * size + (size - 1 - i)];
-}
-printf("%d, %d\n", sum1, sum2);
+    for (i = 0; i < size; i++)
+    {
+        sum1 += a[i * size + i];
+        sum2 += a[i * size + (size - 1 - i)];
+    }
+    printf("%d, %d\n", sum1, sum2);
 }
