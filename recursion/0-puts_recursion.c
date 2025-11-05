e37cd1268b994009172dc66f09ed7e89913f132a
Tchepekino-code <12193@holbertonstudents.com>
2025-11-05 15:36:54 +0100

    000

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@ void _puts_recursion(char *s)
 {
 
 if (!*s)
-{
     _putchar('\n');
-    return;
+else
+{
+    _putchar(*s);
+    _puts_recursion(s + 1);
 }
-
-_putchar(*s);
-_puts_recursion(s + 1);
 }
