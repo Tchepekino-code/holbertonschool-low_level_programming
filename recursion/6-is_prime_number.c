d0235c30ddd83e30e8d090b1428e5c21b2be26a7
Tchepekino-code <12193@holbertonstudents.com>
2025-11-06 13:54:31 +0100

    006

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+# include "main.h"
+
+/**
+ * _is_helper_number - Vérifie si un nombre répond à une condition spécifique
+ *                     (ex : divisibilité, primalité, etc.) de manière récursive.
+ * @n: Le nombre à tester.
+ * @i: L'entier utilisé pour le test récursif (souvent commence à 2).
+ *
+ * Return: 1 si la condition est remplie, 0 sinon.
+ */
+is_helper_number(int n, int i)
+{
+   
+	if (n % i == 0)
+	return (0);
+
+	if (i == n)
+	return (1);
+
+	return (is_helper_number(n, i + 1));
+}
+
+/**
+ * is_prime_number - Vérifie si un nombre est premier.
+ * @n: Le nombre à tester.
+ *
+ * Return: 1 si le nombre est premier, 0 sinon.
+ */
+int is_prime_number(int n)
+{
+	if (n < 1)
+	return (0);
+
+	return (is_helper_number(n, 0));
+}
+
