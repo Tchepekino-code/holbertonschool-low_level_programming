94ad8401576d40705cf2d626f5b7ddcef55fbcda
Tchepekino-code <12193@holbertonstudents.com>
2025-11-06 13:14:54 +0100

    005

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,14 +10,12 @@
 
 int _sqrt_helper(int n, int i)
 {
-	if (n < 0)
-		return (-1);
-	
-	if (n == 0 || n == 1)
-		return (n);
 	
 	if (i * i > n)
 		return (-1);
+	
+	if (i * i == n)
+		return (n);
 	
 		return _sqrt_helper(n, i +1);
 }
@@ -32,6 +30,10 @@ int _sqrt_helper(int n, int i)
 
 int _sqrt_recursion(int n)
 {
+	
+	if (n < 0)
+		return (-1);
+		
 	return (_sqrt_helper(n, 0));
 }
 	
