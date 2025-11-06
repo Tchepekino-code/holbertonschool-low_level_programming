dfda1c6ce7c50ab05632b4ebaf1da6fc41dddad7
Tchepekino-code <12193@holbertonstudents.com>
2025-11-06 14:59:45 +0100

    006

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,17 +10,11 @@
  */
 int is_helper_number(int n, int i)
 {
-	if (n <= 1)
-	return (0);
-
-	if ( n % i != 0)
-	return (0);
-
-	if (n % i == 0  && n != i)
-	return (0);
-
-	if (i == n)
+	if ( i * i > n)
 	return (1);
+
+	if (n % i == 0)
+	return (0);
 
 	return (is_helper_number(n, i + 1));
 }
@@ -33,7 +27,9 @@ int is_helper_number(int n, int i)
  */
 int is_prime_number(int n)
 {
-	
+	if (n <= 1)
+	return (0);
+
 	return (is_helper_number(n, 2));
 }
 
