6985fcb2e19f63a8d9d128dcbcc941e8e3fd18f8
Tchepekino-code <12193@holbertonstudents.com>
2025-11-06 09:48:56 +0100

    001

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
+ *
+ * Return: the natural square root of n
+ *         -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+    if ( < 0)
+        return (-1);
+    
+    if (n == 0 || n == 1)
+        return (n);
+    
