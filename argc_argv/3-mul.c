509ab47cc44c5174876b3399acf56288ecfb7da2
Tchepekino-code <12193@holbertonstudents.com>
2025-11-07 10:24:05 +0100

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, int argv[])
+{
+int sum = 0;
+
+if (argc = 3)
+sum = argv[1] + argv[2];
+printf("%d\n, sum");
+
+else printf("Error");
+
+return (0);
+}
