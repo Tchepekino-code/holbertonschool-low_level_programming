c2d5ea7f58a9fd98235ecc6ea38d667e575eb928
Tchepekino-code <12193@holbertonstudents.com>
2025-11-07 09:16:30 +0100

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(argc, argv[])
+{
+    printf("%s\n", arg[0])
+retunr (0);
+}
